dfbe7e71eb3592441022936bb4a537ecdcbd50ce
Noe TATOUD <noe.tatoud@gmail.com>
2025-12-18 11:17:28 +0100

    feat(docs): Base documentation with fumadocs nextjs

---
+++ apps/docs/rehype-code.config.ts
@@ -0,0 +1,10 @@
+import type { RehypeCodeOptions } from 'fumadocs-core/mdx-plugins';
+
+export const rehypeCodeOptions: RehypeCodeOptions = {
+  themes: {
+    light: 'github-light',
+    dark: 'github-dark',
+  },
+  inline: 'tailing-curly-colon',
+  defaultColor: false,
+};
