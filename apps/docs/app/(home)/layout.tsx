30f1904f0905b8113c76cccdf4773983c468ea47
Noe TATOUD <noe.tatoud@gmail.com>
2025-12-18 16:06:04 +0100

    feat: add github links

--- apps/docs/app/(home)/layout.tsx
+++ apps/docs/app/(home)/layout.tsx
@@ -1,6 +1,11 @@
 import { HomeLayout } from 'fumadocs-ui/layouts/home';
 import { baseOptions } from '@/lib/layout.shared';
+import { GithubInfo } from 'fumadocs-ui/components/github-info';
 
 export default function Layout({ children }: LayoutProps<'/'>) {
-  return <HomeLayout {...baseOptions()}>{children}</HomeLayout>;
+  return (
+    <HomeLayout {...baseOptions()} githubUrl="https://github.com/ntatoud/base-vaul">
+      {children}
+    </HomeLayout>
+  );
 }
--- apps/docs/app/docs/layout.tsx
+++ apps/docs/app/docs/layout.tsx
@@ -1,10 +1,20 @@
 import { source } from '@/lib/source';
 import { DocsLayout } from 'fumadocs-ui/layouts/docs';
 import { baseOptions } from '@/lib/layout.shared';
+import { GithubInfo } from 'fumadocs-ui/components/github-info';
 
 export default function Layout({ children }: LayoutProps<'/docs'>) {
   return (
-    <DocsLayout tree={source.pageTree} {...baseOptions()}>
+    <DocsLayout
+      tree={source.pageTree}
+      {...baseOptions()}
+      links={[
+        {
+          type: 'custom',
+          children: <GithubInfo owner="ntatoud" repo="base-vaul" className="lg:-mx-2" />,
+        },
+      ]}
+    >
       {children}
     </DocsLayout>
   );
